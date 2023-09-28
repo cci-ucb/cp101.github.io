1fa303339177f4344fe154f9d98e960a2f90e2b7
Hannah Moore <hmoore@laffont.Berkeley.EDU>
2023-09-28 10:42:58 -0700

    set up utility functions; predownload data

--- labs/lab07/ckan_utils.py
+++ labs/lab07/ckan_utils.py
@@ -3,55 +3,6 @@
 These functions have been adapted from Data 100 course materials.
 """
 
-def load_keys(path):
-    """Loads your Twitter authentication keys from a file on disk.
-    
-    Args:
-        path (str): The path to your key file.  The file should
-          be in JSON format and look like this (but filled in):
-            {
-                "consumer_key": "<your Consumer Key here>",
-                "consumer_secret":  "<your Consumer Secret here>",
-                "access_token": "<your Access Token here>",
-                "access_token_secret": "<your Access Token Secret here>"
-            }
-    
-    Returns:
-        dict: A dictionary mapping key names (like "consumer_key") to
-          key values."""
-    
-    import json
-    with open(path) as f:
-        keys = json.load(f)
-    return keys
-
-def validate_authentication(keys):
-    """Tests that keys work. 
-    
-    Args:
-        keys (dict): A Python dictionary with Twitter authentication
-          keys (strings), like this (but filled in):
-            {
-                "consumer_key": "<your Consumer Key here>",
-                "consumer_secret":  "<your Consumer Secret here>",
-                "access_token": "<your Access Token here>",
-                "access_token_secret": "<your Access Token Secret here>"
-            }
-    
-    Returns:
-        Nothing, prints out a message based on if the keys are valid or not.
-    """
-    import tweepy
-    import logging
-    try:
-        auth = tweepy.OAuthHandler(keys["consumer_key"], keys["consumer_secret"])
-        auth.set_access_token(keys["access_token"], keys["access_token_secret"])
-        api = tweepy.API(auth)
-        print("The keys are valid. Your username is:", api.get_lists())
-    except tweepy.TweepyException as e:
-        logging.warning("There was a Tweepy error. Double check your API keys and try again.")
-        logging.warning(e)
-    
 def download_recent_tweets_by_user(user_account_name, keys):
     """Downloads tweets by one Twitter user.
 
@@ -110,14 +61,14 @@ def download_recent_tweets_by_hashtag(hashtag, keys, location=None, count=15):
         logging.warning(e)
     return tweets
 
-def save_tweets(tweets, path):
-    """Saves a list of tweets to a file in the local filesystem. Extension
+def write_data(dataset, path):
+    """Saves a list of data to a file in the local filesystem. Extension
     must be .json.
 
     Args:
-        tweets (list): A list of tweet objects (of type Dictionary) to
+        dataset (list): A list of data objects (of type Dictionary) to
           be saved.
-        path (str): The place where the tweets will be saved.
+        path (str): The place where the data will be saved.
 
     Returns:
         None"""
@@ -125,48 +76,21 @@ def save_tweets(tweets, path):
     with open(path, "w") as f:        
         json.dump(tweets, f)
         
-def load_tweets(path):
-    """Loads tweets that have previously been saved as a json file.
+def read_data(path):
+    """Loads data that have previously been saved as a json file.
     
-    Calling load_tweets(path) after save_tweets(tweets, path)
+    Calling read_data(path) after write_data(dataset, path)
     will produce the same list of tweets.
     
     Args:
-        path (str): The place where the tweets were be saved.
+        path (str): The place where the data were saved.
 
     Returns:
-        list: A list of Dictionary objects, each representing one tweet."""
+        list: A list of Dictionary objects, each representing one dataset."""
     import json
     with open(path, "r") as f:
         tweets = json.load(f)
     return tweets
-
-def get_user_tweets_with_cache(user_account_name, keys_path):
-    """Get recent tweets from one user, loading from a disk cache if available.
-    
-    The first time you call this function, it will download tweets by
-    a user.  Subsequent calls will not re-download the tweets; instead
-    they'll load the tweets from a save file in your local filesystem.
-    All this is done using the functions you defined in the previous cell.
-    This has benefits and drawbacks that often appear when you cache data:
-    
-    +: Using this function will prevent extraneous usage of the Twitter API.
-    +: You will get your data much faster after the first time it's called.
-    -: If you really want to re-download the tweets (say, to get newer ones,
-       or because you screwed up something in the previous cell and your
-       tweets aren't what you wanted), you'll have to find the save file
-       (which will look like <something>_recent_tweets.pkl) and delete it.
-    
-    Args:
-        user_account_name (str): The Twitter handle of a user, without the @.
-        keys_path (str): The path to a JSON keys file in your filesystem.
-    """
-    ds_tweets_save_path = '{}_recent_tweets.pkl'.format(user_account_name)
-    if not Path(ds_tweets_save_path).is_file():
-        tweets = download_recent_tweets_by_user(user_account_name, load_keys(keys_path))
-        save_tweets(tweets, ds_tweets_save_path)
-        
-    return load_tweets(ds_tweets_save_path)
 
 def load_vader():
     """Returns a DataFrame of the VADER sentiment lexicon. Row indices correspond
@@ -182,7 +106,7 @@ def load_vader():
     sent['polarity'] = sent['polarity'].apply(float)
     return sent
 
-def clean_tweets(s):
+def clean_string(s):
     """Lowercase and remove punctuation from a pd.Series object containing text.
     
     Args:
